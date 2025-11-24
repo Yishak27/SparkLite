836f7f420226b06609d8959296fffff146365b4d
PC <yisumaid3@gmail.com>
2025-11-24 14:27:34 +0300

    - some adjustment for json add json parser and seroalozer

--- main.py
+++ main.py
@@ -6,35 +6,55 @@ import io
 max_file_size = 20 * 1024 * 1024 
 st.title("SparkLite AI")
 st.header("This is a simple AI agent that do sales analysis.")
-
+df = None
 st.set_page_config(
     page_title="SparkLite AI",
     layout="wide",
     page_icon=">"
 )
 
-if 'uploaded_data' not in st.session_state:
+if 'uploaded_data' not in st.session_state or 'file_name' not in st.session_state:
     st.session_state.uploaded_data = None
-if 'file_name' not in st.session_state:
     st.session_state.file_name = None
-uploaded_file = st.file_uploader("Upload a CSV file",help=None,type=["csv","json"], key="uploaded_file")
-if uploaded_file is not None:
-    if uploaded_file.size > max_file_size:
-       st.error(f"File size is too large. Max file size is {max_file_size} bytes.")
-    else :
-        st.session_state.uploaded_data = uploaded_file.read()
-        st.session_state.file_name = uploaded_file.name
-        
-if st.session_state.uploaded_data is not None:
-    try:
-        if st.session_state.file_name.endswith(".csv"):
-            df = pd.read_csv(io.BytesIO(st.session_state.uploaded_data))
-            st.line_chart(df, x="product", y="total_price") 
-        elif st.session_state.file_name.endswith(".json"):
-            df = pd.read_json(io.BytesIO(st.session_state.uploaded_data))
-            st.line_chart(df, x="product", y="total_price") 
-        else:
-            st.error("Unsupported file format.")
-    except Exception as e:
-        print('erro reading file:', e)
-        st.error(f"Error reading file:")
+uploaded_file = st.file_uploader(
+    "Upload a CSV or JSON file",
+    help=None,
+    type=["csv","json"],
+    key="uploaded_file"
+)
+try: 
+    if uploaded_file is not None:
+        if uploaded_file.size > max_file_size:
+            st.error(f"File size is too large. Max file size is {max_file_size} bytes.")            
+    if st.session_state.uploaded_data is not None:
+        try:
+            if st.session_state.file_name.endswith(".csv"):
+                df = pd.read_csv(uploaded_file)
+            elif st.session_state.file_name.endswith(".json"):
+                if isinstance(json_data, list):
+                    df = pd.DataFrame(json_data)
+                elif isinstance(json_data, dict):
+                    print('readed data, ', json_data)
+                    if 'data' in json_data and isinstance(json_data['data'], list):
+                        df = pd.DataFrame(json_data['data'])
+                    else:
+                        df = pd.DataFrame([json_data])
+                else:
+                    st.error("Unsupported JSON structure")
+                    df = None
+                st.session_state.uploaded_data = df
+            else:
+                st.error("Unsupported file format.")
+            
+        except Exception as e:
+            print('erro reading file:', e)
+            st.error(f"Error reading file:")
+            st.session_state.uploaded_data = None
+            st.session_state.file_name = None
+    if df is not None:
+        st.line_chart(df, x="product", y="total_price") 
+except Exception as e:
+    print('erro reading file:', e)
+    st.error(f"Error reading file:")
+    st.session_state.uploaded_data = None
+    st.session_state.file_name = None  
