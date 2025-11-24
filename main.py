61e292581a6e2c7a6489e78fa0b42586f72383df
PC <yisumaid3@gmail.com>
2025-11-24 13:50:24 +0300

    - add file uploader for streamlite

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import streamlit as st
 import pandas as pd
+import json
  
 st.title("SparkLite AI")
 st.header("This is a simple AI agent that do sales analysis.")
-st.write("Welcome to SparkLite AI")
- 
-df = pd.read_csv("sales_data.csv")
-st.line_chart(df, x="product", y="total_price")
+
+st.set_page_config(
+    page_title="SparkLite AI",
+    layout="wide",
+    page_icon=">"
+)
+
+if 'uploaded_data' not in st.session_state:
+    st.session_state.uploaded_data = None
+if 'file_name' not in st.session_state:
+    st.session_state.file_name = None
+st.file_uploader("Upload a CSV file", type="csv,json", key="uploaded_file")
+if st.session_state.uploaded_file is not None:
+    st.session_state.uploaded_data = st.session_state.uploaded_file.read()
+    st.session_state.file_name = st.session_state.uploaded_file.name
+
+st.write("file uploaded", st.session_state.file_name)
+st.write("file data", st.session_state.uploaded_data)
+if st.session_state.uploaded_data is not None:
+    df = pd.read_csv(st.session_state.uploaded_data)
+    st.line_chart(df, x="product", y="total_price")
