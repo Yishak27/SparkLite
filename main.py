871a79ce5ad8e432dddf2a4a734b261c16cbd958
PC <yisumaid3@gmail.com>
2025-11-24 13:58:23 +0300

    - adding file reader and show table from uploaded data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import json
+import io 
  
 st.title("SparkLite AI")
 st.header("This is a simple AI agent that do sales analysis.")
@@ -15,13 +16,16 @@ if 'uploaded_data' not in st.session_state:
     st.session_state.uploaded_data = None
 if 'file_name' not in st.session_state:
     st.session_state.file_name = None
-st.file_uploader("Upload a CSV file", type="csv,json", key="uploaded_file")
-if st.session_state.uploaded_file is not None:
-    st.session_state.uploaded_data = st.session_state.uploaded_file.read()
-    st.session_state.file_name = st.session_state.uploaded_file.name
+uploaded_file = st.file_uploader("Upload a CSV file", type=["csv","json"], key="uploaded_file")
+if uploaded_file is not None:
+    st.session_state.uploaded_data = uploaded_file.read()
+    st.session_state.file_name = uploaded_file.name
 
 st.write("file uploaded", st.session_state.file_name)
-st.write("file data", st.session_state.uploaded_data)
+# st.write("file data", st.session_state.uploaded_data)
 if st.session_state.uploaded_data is not None:
-    df = pd.read_csv(st.session_state.uploaded_data)
+    try:
+        df = pd.read_csv(io.BytesIO(st.session_state.uploaded_data))
+    except Exception as e:
+        st.error(f"Error reading file: {e}")
     st.line_chart(df, x="product", y="total_price")
